e80811297edcb8db7a9701fbed2ce95245cdf940
501urchin <jaychhitu@gmail.com>
2026-03-19 14:35:29 +0100

    addded some const helpers

--- pkg/bits/helpers.go
+++ pkg/bits/helpers.go
@@ -3,7 +3,12 @@ package bits
 
 import "fmt"
 
-// At gets the bit at b[idx]
+const (
+	BitZero = 0x00
+	BitOne  = 0x01
+)
+
+// At gets the bit at b[idx] and returns it
 func At(b byte, idx int) byte {
 	if idx > 7 || idx < 0 {
 		panic(fmt.Errorf("invalid bit index %d of 7", idx))
@@ -12,7 +17,7 @@ func At(b byte, idx int) byte {
 	return (b >> idx) & 1
 }
 
-// Flip attempts to flip a bit at b[idx].
+// Flip attempts to flip a bit at b[idx] and returns the new byte
 func Flip(b byte, idx int) byte {
 	if idx > 7 || idx < 0 {
 		panic(fmt.Errorf("invalid bit index %d of 7", idx))
